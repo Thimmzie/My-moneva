b996ab1e44cfe3a334b0115e66c8ae40c0bb49d6
Ayodeji <thimmzieayodeji@gmail.com>
2025-10-01 12:35:09 +0100

    testimonial update

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -95,54 +95,53 @@ const testimonials = () => {
           What people say about us
         </h2>
         <p className="text-[#8f8f8f] text-center text-[1rem] md:text-[1.2rem] lg:text-[1.1rem] xl:text-[1rem] testimnial-maximize font-bold lg:font-light mx-2">
-          Don’t just take our word for it hear from our users
-          <br className="block lg:hidden"></br> about their
+          Don’t just take our word for it hear from our users about their
           <br className="hidden lg:block"></br> experience with moneva
         </p>
       </div>
       <div className="mt-10 bg-[url('./assets/images/mobilemap.png')] bg-cover h-[80vh] bg-no-repeat relative lg:bg-[url('./assets/images/SeekPng.png')] lg:mx-[6rem] ellipse-container">
         <div>
           <img
-            className="absolute w-[5%]  left-[2%] lg:w-[2.7%] lg:left-[10%] lg:top-[3%] ellipse"
+            className="absolute w-[8%]  left-[2%] lg:w-[2.7%] lg:left-[10%] lg:top-[3%] ellipse"
             src={EllipseSix}
           />
           <img
-            className="absolute w-[7%] top-[10%] left-[20%] lg:w-[3.8%] lg:left-[25%] lg:top-[15%] ellipse"
+            className="absolute w-[10%] top-[10%] left-[20%] lg:w-[3.8%] lg:left-[25%] lg:top-[15%] ellipse"
             src={EllipseFour}
           />
           <img
-            className="absolute w-[4%] top-[17%] left-[60%] lg:w-[2.2%] lg:left-[68%] lg:top-[22%] ellipse"
+            className="absolute w-[7%] top-[17%] left-[60%] lg:w-[2.2%] lg:left-[68%] lg:top-[22%] ellipse"
             src={EllipseEight}
           />
           <img
-            className="absolute w-[7%] top-[2%] left-[85%] lg:w-[3.8%] lg:left-[83%] lg:top-[3%] ellipse"
+            className="absolute w-[10%] top-[2%] left-[85%] lg:w-[3.8%] lg:left-[83%] lg:top-[3%] ellipse"
             src={EllipseTwo}
           />
           <img
-            className="absolute w-[12%] top-[30%] left-[45%] lg:w-[6%] lg:left-[50%] ellipse"
+            className="absolute w-[19%] top-[30%] left-[45%] lg:w-[6%] lg:left-[50%] ellipse"
             src={EllipseOne}
           />
           <img
-            className="absolute w-[5%] top-[58%] left-[12%] lg:w-[2.7%] lg:left-[22%] ellipse"
+            className="absolute w-[9%] top-[58%] left-[12%] lg:w-[2.7%] lg:left-[22%] ellipse"
             src={EllipseSeven}
           />
           <img
-            className="absolute w-[4%] top-[48%] left-[30%] lg:w-[2.2%] lg:left-[36%] ellipse"
+            className="absolute w-[7%] top-[48%] left-[30%] lg:w-[2.2%] lg:left-[36%] ellipse"
             src={EllipseNine}
           />
           <img
-            className="absolute w-[5%] top-[52%] left-[75%] lg:w-[2.7%] ellipse"
+            className="absolute w-[8%] top-[52%] left-[75%] lg:w-[2.7%] ellipse"
             src={EllipseFive}
           />
           <img
-            className="absolute w-[7%] top-[35%] left-[93%] lg:w-[3.8%] lg:left-[88%] lg:top-[33%] ellipse"
+            className="absolute w-[10%] top-[35%] left-[90%] lg:w-[3.8%] lg:left-[88%] lg:top-[33%] ellipse"
             src={EllipseThree}
           />
         </div>
         <Swiper>
           <SwiperSlide>
-            <div className="bg-[#ffff] w-[80%] h-[40vh] review-box mx-auto mt-[28rem] mb-[2rem] flex flex-col items-center justify-center gap-5 px-6 ">
-              <p className="text-[#595959] font-light tracking-wide  text-[1rem] text-center leading-7">
+            <div className="bg-[#ffff] w-[75%] h-[37vh] review-box mx-auto mt-[28rem] mb-[2rem] flex flex-col items-center justify-center gap-3 px-6 ">
+              <p className="text-[#595959] font-light tracking-wide  text-[0.9rem] text-center leading-7">
                 Our platform uses blockchain technology to create,
                 <br className="hidden sm:block"></br>
                 manage, and validate event tickets. This ensures each
@@ -151,7 +150,7 @@ const testimonials = () => {
                 <br className="hidden sm:block"></br> preventing fraud and
                 scalping.
               </p>
-              <h4 className="text-[#2b2929] font-bold text-[1.1rem]">
+              <h4 className="text-[#2b2929] font-bold text-[1rem]">
                 Adetayo Johnson
               </h4>
               <img src={Star} />
