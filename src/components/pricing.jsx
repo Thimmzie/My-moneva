7b47b04a8c207295b18b0046bf426abf65eba984
Ayodeji <thimmzieayodeji@gmail.com>
2025-10-04 13:09:03 +0100

    pricing commenced

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { FaCheck } from 'react-icons/fa';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 
@@ -58,6 +59,61 @@ const pricing = () => {
           <br className="hidden lg:block"></br>
           there’s a plan for everyone
         </p>
+      </div>
+      <div className="toggle"></div>
+      <div className="mt-[5rem]">
+        <div className="monthly flex flex-col gap-4 items-center">
+          <div className="bg-[#f6f6f6] w-[90vw] h-[78vh] rounded-3xl pt-[3rem] px-[1.2rem]">
+            <h2 className="pricing-box-title">Free</h2>
+            <p className="text-[1.2rem] text-[#808080] py-8 font-bold">
+              Ideal for beginners who want to test <br></br> the waters with
+              simple budgeting tools.
+            </p>
+            <div className=" w-full relative border-div">
+              <div className="sub-border-div">
+                <p className="text-[#595959] text-[1rem]">
+                  &#8358;
+                  <span className="pl-1 font-extrabold text-[#272727] text-[3rem]">
+                    0
+                  </span>
+                  <span className="text-[1.2rem]">/month</span>
+                </p>
+                <button className="bg-[#8a66f1] text-[#ffffff] border-0 rounded-[5rem] px-7 h-[6vh]">
+                  Get started
+                </button>
+              </div>
+            </div>
+            <div className="mt-[2rem]">
+              <ul className="flex flex-col gap-5">
+                <div className="flex gap-4 ">
+                  <div className="bg-[#8a66f1] rounded-full inline-flex items-center justify-center">
+                    <FaCheck className=" off-white-check m-2 text-[#ffff]" />
+                  </div>
+                  <p className="text-[1.2rem] text-[#595959] tracking-wide">
+                    Customizable Budgets
+                  </p>
+                </div>
+                <div className="flex gap-4 ">
+                  <div className="bg-[#8a66f1] rounded-full inline-flex items-center justify-center">
+                    <FaCheck className=" off-white-check m-2 text-[#ffff]" />
+                  </div>
+                  <p className="text-[1.2rem] text-[#595959] tracking-wide">
+                    Savings Goal Tracker
+                  </p>
+                </div>
+                <div className="flex gap-4 ">
+                  <div className="bg-[#8a66f1] rounded-full inline-flex items-center justify-center">
+                    <FaCheck className=" off-white-check m-2 text-[#ffff]" />
+                  </div>
+                  <p className="text-[1.2rem] text-[#595959] tracking-wide">
+                    Basic Customer Support
+                  </p>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="annually"></div>
       </div>
     </div>
   );
